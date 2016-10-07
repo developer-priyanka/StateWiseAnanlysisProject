118319d986b3dac7900274383cab1db9e8477d94
developer-priyanka <priyankajain242003@gmail.com>
2016-10-07 15:39:50 +0530

    Update CheckBPLForEightyPercent.java

--- CheckBPLForEightyPercent.java
+++ CheckBPLForEightyPercent.java
@@ -11,14 +11,14 @@ public class CheckBPLForEightyPercent extends EvalFunc<String> {
 	@Override
 	public String exec(Tuple arg0) throws IOException {
 		
-		if(arg0==null|| arg0.size()==0)
+		if(arg0==null|| arg0.size()==0)      //checking if inputs are null or 0.
 			return "";
 		int numofinputs=arg0.size();
 		if(numofinputs<2)
-			return "";
-		long x=Long.parseLong((String)arg0.get(0)); //objective BPL
-		long y=Long.parseLong((String)arg0.get(1)); //performance BPL
-		long z=(long)(x*80)/100; //80% of BPL objectives
+			return "";  
+		long x=Long.parseLong((String)arg0.get(0)); //objective of BPL Crad
+		long y=Long.parseLong((String)arg0.get(1)); //performance on BPL Card
+		long z=(long)(x*80)/100; //calculating 80% of BPL objectives
 		
 		if(y>=z) //compare objectives with performance
 			return "Yes";
